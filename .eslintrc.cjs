6b67307533e24625f7bda7b1fe3d7c81be179d9f
otter0104 <otter0104@innodep.com>
2023-08-25 13:48:54 +0900

    🎉 Feat: "First Commit"
    Front-End 기본 환경 구성 프로젝트 Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', "prettier","import"],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "import/order": [
+      "error",
+      {
+          alphabetize: {
+              order: "asc",
+              caseInsensitive: true,
+          },
+          "newlines-between": "always",
+          groups: ["builtin", "external", "parent", "sibling", "index"],
+          pathGroups: [
+              {
+                  pattern: "react",
+                  group: "external",
+                  position: "before",
+              },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto",
+            },
+        ],
+      },
+  ],
+  "no-unused-vars": "off",
+  },
+}
