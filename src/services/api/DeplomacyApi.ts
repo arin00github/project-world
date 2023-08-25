468e7a2b27580f1c9989371de19a6614ab28bf1c
otter0104 <otter0104@innodep.com>
2023-08-25 17:08:20 +0900

    ✨ Feat: 외교정보API 작업
    국기이미지API작업
    경제정보API작업
    환경정보API작업
    지도이미지API작업
    기본정보API작업

---
+++ src/services/api/DeplomacyApi.ts
@@ -0,0 +1,44 @@
+import {
+  ICountryEconomy,
+  ICountryEnv,
+  ICountryMap,
+  IDeplomacyList,
+  IOneFilter,
+  IResultCode,
+} from "../../interfaces/deplomacy-interface";
+
+import { DeplomacyHttp } from "./DeplomacyHttp";
+
+export interface IDeplomacyApi {
+  GetNationInfoList(
+    pageIndex: number,
+  ): Promise<IResultCode<IDeplomacyList> | undefined>;
+
+  GetTotalCountry(
+    pageNo: number,
+    numOfRow?: number,
+  ): Promise<IResultCode<IDeplomacyList> | undefined>;
+  GetCountryInfo(
+    options: IOneFilter,
+  ): Promise<IResultCode<IDeplomacyList> | undefined>;
+  GetCountryMap(country: {
+    iso: string;
+    name: string;
+  }): Promise<IResultCode<ICountryMap> | undefined>;
+  GetCountryEnv(country: {
+    iso: string;
+    name: string;
+  }): Promise<IResultCode<ICountryEnv> | undefined>;
+  GetCountryFlag(country: {
+    iso: string;
+    name: string;
+  }): Promise<IResultCode<ICountryMap> | undefined>;
+  GetDetailEconomy(country: {
+    iso: string;
+    name: string;
+  }): Promise<IResultCode<ICountryEconomy> | undefined>;
+}
+
+export const NewDeplomacyApi = (): IDeplomacyApi => {
+  return new DeplomacyHttp();
+};
