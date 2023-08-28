6ef6110106695299d8cb9f92bc18ad3dff710944
otter0104 <otter0104@innodep.com>
2023-08-28 14:07:52 +0900

    ✨ Feat: Notion API를 연결
    Notion API를 Cloudflare에서 우회해서 받도록 설정

--- src/pages/archive/index.tsx
+++ src/pages/archive/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import {
   FloatContainer,
   FloatContainerOverlay,
@@ -8,6 +10,23 @@ import { MapBox } from "../../components/world";
 import { Box } from "@chakra-ui/react";
 
 const ArchivePage = () => {
+  const testAPI = async () => {
+    const bodyData = {
+      database_id: "54e82072fbff4590a759c86d86670d05",
+    };
+    const res = await fetch(
+      "https://project-world-archive.teru325.workers.dev",
+      {
+        body: JSON.stringify(bodyData),
+        method: "GET",
+      },
+    );
+    const data = await res.json();
+    console.log("data", data);
+  };
+  useEffect(() => {
+    testAPI();
+  }, []);
   return (
     <Box>
       <MapBox />
